0e8f4fafaf3aa2f18d6064cf135978a821ad896f
KhailoAndrey <hap512@gmail.com>
2023-02-06 00:25:59 +0200

    work lightbox

--- src/index.js
+++ src/index.js
@@ -1,30 +1,22 @@
 import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
 import axios from 'axios';
-// import { findLastIndex } from 'lodash';
+
 const userId = '33355093-a15ac59f0161a10cfc7b50674';
 const requestUrl = 'https://pixabay.com/api/?';
+
 const emptyRequest =
   'Sorry, there are no images matching your search query. Please try again.';
 const endOfRequest =
   "We're sorry, but you've reached the end of search results.";
+
 const formRequest = document.querySelector('#search-form');
 const galleryDesk = document.querySelector('.gallery');
 const loadBtn = document.querySelector('.load-more');
-// const findText = '';
-let totalHits = 0;
-// const totalImagesInRequest = `'Hooray! We found ${totalHits} images.'`;
+
 let numPage = 1;
 const per_page = 40;
-let responseArray = [];
-let webformatURL = null;
-let largeImageURL = null;
-let tags = null;
-let likes = 0;
-let views = 0;
-let comments = 0;
-let downloads = 0;
 
 formRequest.addEventListener('submit', e => {
   e.preventDefault();
@@ -33,7 +25,6 @@ formRequest.addEventListener('submit', e => {
   console.log(findText);
 
   async function getImage() {
-    // try {
     const request = await axios({
       url: `${requestUrl}`,
       params: {
@@ -46,18 +37,19 @@ formRequest.addEventListener('submit', e => {
         page: numPage,
       },
     });
-    totalHits = request.data.totalHits;
+    const totalHits = request.data.totalHits;
     const pageGroup = totalHits / per_page;
     console.log(pageGroup);
     responseArray = [];
-    for (const elem of request.data.hits) {
-      webformatURL = elem.webformatURL;
-      largeImageURL = elem.largeImageURL;
-      tags = elem.tags;
-      likes = elem.likes;
-      views = elem.views;
-      comments = elem.comments;
-      downloads = elem.downloads;
+    for (const {
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    } of request.data.hits) {
       responseArray.push({
         webformatURL,
         largeImageURL,
@@ -73,18 +65,14 @@ formRequest.addEventListener('submit', e => {
     // return request.data;
   }
   getImage().then(() => {
-    numPage += 1;
-    loadBtn.removeAttribute('hidden');
-
     createCardImage(responseArray);
-    console.log;
+    galleryLightBox();
   });
 });
 function createCardImage(responseArray) {
   const markup = responseArray
     .map(
-      arrItem => `
-       
+      arrItem => `       
       <a class="image-link" href="${arrItem.largeImageURL}">
         <div class="photo-card">
         <img class="gallery-image" src="${arrItem.webformatURL}" alt="${arrItem.tags}" loading="lazy" width=320px height=215px/>
@@ -132,15 +120,13 @@ ${arrItem.downloads}
     .join('');
   galleryDesk.innerHTML = markup;
 }
-let gallery = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
 
-galleryDesk.addEventListener('click', event => {
-  event.preventDefault();
-  gallery.on('show.Simplelightbox');
-});
+function galleryLightBox() {
+  return new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
 
 function emptyMessage() {
   Notiflix.Notify.info(`${emptyRequest}`);
